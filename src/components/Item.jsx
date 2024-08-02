7db9807494e9d03c7ad4fc39816085eae75b74cc
priyanka <priyankachikhale7@gmail.com>
2024-08-02 20:59:29 +0530

    Initial implementation of 'Add to Cart' button with console logging

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,6 +2,7 @@ import Icon from "../assets/images/icon-add-to-cart.svg";
 import Increment from "../assets/images/icon-increment-quantity.svg";
 import Decrement from "../assets/images/icon-decrement-quantity.svg";
 import { useState } from "react";
+import id from "../data.json"
 
 const Item = ({ img, category, name, price }) => {
 	const [count, setCount] = useState(0);
@@ -21,6 +22,8 @@ const Item = ({ img, category, name, price }) => {
 	};
 
 	const addToCart = () => {
+		console.log(id);
+		console.log(name);
 		setIsInCart(true);
 		setCount(1);
 	};
@@ -55,6 +58,7 @@ const Item = ({ img, category, name, price }) => {
 					<button
 						className="flex items-center justify-center px-6 py-2 bg-white border rounded-full border-orange-950"
 						onClick={addToCart}
+						data-productId={id}
 					>
 						<img src={Icon} alt="Add to Cart Icon" className="w-5 h-5" />
 						<h2 className="ml-2 text-xs font-semibold">Add to Cart</h2>
